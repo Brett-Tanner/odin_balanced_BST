b920b9af625ff3be729fde0139341c14a28f579d
Brett Tanner <bretttanner171@gmail.com>
2022-08-13 15:08:16 +0900

    Add spaces for readability

--- run.rb
+++ run.rb
@@ -7,13 +7,13 @@ test.pretty_print
 puts "The tree is balanced!" if test.balanced?
 
 puts "Here are the elements in level order:"
-test.level_order {|node| print node.data}
+test.level_order {|node| print "#{node.data} "}
 puts "\nHere are the elements in order:"
-test.inorder {|node| print node.data}
+test.inorder {|node| print "#{node.data} "}
 puts "\nHere are the elements in preorder:"
-test.preorder {|node| print node.data}
+test.preorder {|node| print "#{node.data} "}
 puts "\nHere are the elements in postorder:"
-test.postorder {|node| print node.data}
+test.postorder {|node| print "#{node.data} "}
 puts ""
 
 15.times {|i| test.insert(rand(101..200) + i)}
@@ -29,10 +29,10 @@ test.pretty_print
 puts "You rebalanced the tree!" if test.balanced?
 
 puts "Here are the elements for the rebalanced array in level order:"
-test.level_order {|node| print node.data}
+test.level_order {|node| print "#{node.data} "}
 puts "\nHere are the elements for the rebalanced array in order:"
-test.inorder {|node| print node.data}
+test.inorder {|node| print "#{node.data} "}
 puts "\nHere are the elements for the rebalanced array in preorder:"
-test.preorder {|node| print node.data}
+test.preorder {|node| print "#{node.data} "}
 puts "\nHere are the elements for the rebalanced array in postorder:"
-test.postorder {|node| print node.data}
+test.postorder {|node| print "#{node.data} "}
